0b8b2398284e598a6cfd3d145cccf8e033d623b6
ZIMMY66 <shubhamkp29@gmail.com>
2026-06-29 12:05:13 +0530

    Day 4: Completed Spring Boot REST API fundamentals

--- src/main/java/com/example/demo/Student.java
+++ src/main/java/com/example/demo/Student.java
@@ -1,28 +1,39 @@
-
-
-
 package com.example.demo;
+
+// This class represents a Student object.
+// It is also called a Model or POJO (Plain Old Java Object).
 
 public class Student {
 
+    // Instance variables (properties of a student)
     private int id;
     private String name;
 
+    // Default Constructor
+    // Spring uses this to create the object automatically.
     public Student() {
     }
 
+    // Getter method for id
+    // Used to get/read the value of id.
     public int getId() {
         return id;
     }
 
+    // Setter method for id
+    // Used to set/update the value of id.
     public void setId(int id) {
         this.id = id;
     }
 
+    // Getter method for name
+    // Used to get/read the value of name.
     public String getName() {
         return name;
     }
 
+    // Setter method for name
+    // Used to set/update the value of name.
     public void setName(String name) {
         this.name = name;
     }
--- src/main/java/com/example/demo/StudentController.java
+++ src/main/java/com/example/demo/StudentController.java
@@ -1,9 +1,5 @@
 package com.example.demo;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class StudentController {
@@ -19,4 +15,22 @@ public class StudentController {
     {
         return "Student Added Successfully";
     }
+    //handles dynamic URL values like /studentss/1
+    @GetMapping("/student/{id}")
+    public String getStudent(
+         //takes values from url and stores
+            @PathVariable int id
+    )
+    {
+        return " Student Id is "+id;
+    }
+    @GetMapping ("/hellobyname")
+    public String hello(
+            @RequestParam String name
+
+    )
+    {
+        return "Hello"+name;
+    }
+
 }
